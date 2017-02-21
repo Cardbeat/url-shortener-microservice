278cfa3aa3a148d736c306bac5227d8664698616
jefferson oliveira <cardbeatt@gmail.com>
2017-02-21 19:27:26 -0300

    things to do tomorrow

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express= require('express');
 const app = express();
 
 
+
+// relearn mongoose trought net ninja
+// try to make an actual app with post/get rotes and return the url 
+// this one will be long but rewarding 
+
 app.listen(3000, () => {
   console.log('running at 3000');
 });
