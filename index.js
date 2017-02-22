dbd1faf35b2c968c585622a3dbd9678374243424
jefferson oliveira <cardbeatt@gmail.com>
2017-02-22 11:49:00 -0300

    structure

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 // try to make an actual app with post/get rotes and return the url 
 // this one will be long but rewarding 
 
+app.set(views, './app/views');
+app.set('view engine', 'pug');
+
+
 app.listen(3000, () => {
   console.log('running at 3000');
 });
